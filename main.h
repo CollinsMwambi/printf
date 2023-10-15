54eff7ec8a0a0c7e4e93f99a866c776a49f9b31a
CollinsMwambi <collins.mwambi@strathmore.edu>
2023-10-15 11:47:53 -0700

    headerFile

--- main.h
+++ main.h
@@ -1,41 +1,22 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-	int len;
-	int len2;
-	unsigned int ui;
-	void *addr;
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
 
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
-	ui = (unsigned int)INT_MAX + 1024;
-	addr = (void *)0x7ffe637541f0;
-	_printf("Length:[%d, %i]\n", len, len);
-	printf("Length:[%d, %i]\n", len2, len2);
-	_printf("Negative:[%d]\n", -762534);
-	printf("Negative:[%d]\n", -762534);
-	_printf("Unsigned:[%u]\n", ui);
-	printf("Unsigned:[%u]\n", ui);
-	_printf("Unsigned octal:[%o]\n", ui);
-	printf("Unsigned octal:[%o]\n", ui);
-	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
-	_printf("Address:[%p]\n", addr);
-	printf("Address:[%p]\n", addr);
-	len = _printf("Percent:[%%]\n");
-	len2 = printf("Percent:[%%]\n");
-	_printf("Len:[%d]\n", len);
-	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
-	return (0);
-}
+int _putchar(char character);
+int printf_char(va_list args, int printed);
+int printf_string(va_list args, int printed);
+int printf_integer(va_list args, int printed);
+int selector(const char *format, va_list args, int printed);
+int printf_binary(unsigned int num, int printed);
+int _printf(const char *format, ...);
+int _x(unsigned int num, int printed, int uppercase);
+int printf_octal(unsigned int num, int printed);
+int printf_unsigned(unsigned int num, int printed);
+int printf_reverse(va_list args, int printed);
+int printf_pointer(va_list args, int printed);
+
+#endif
