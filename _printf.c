2cf2c35265650c1baf43dd1daa00ac17fa6d5fc8
root <root@Cerberus>
2023-10-16 09:50:19 +0300

    Remove the Selector Function & Add Handling for %s and NULL Strings

--- _printf.c
+++ _printf.c
@@ -1,9 +1,52 @@
 #include "main.h"
 
 /**
+ * _print_char - Helper function to print a character
+ * @args: A `va_list` containing the character to print
+ *
+ * This function takes a `va_list` containing the character to print
+ * and prints it to the standard output.
+ *
+ * Return: Number of characters printed (always 1)
+ */
+int _print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * _print_string - Helper function to print a string or "(null)"
+ * @str: The string to print
+ *
+ * Return: Number of characters printed
+ */
+
+int _print_string(char *str)
+{
+	int printed = 0;
+
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	while (*str)
+	{
+		_putchar(*str);
+		printed++;
+		str++;
+	}
+
+	return (printed);
+}
+
+/**
  * _printf - implementation of the inbuilt printf
  * @format: the format specifier
- * Return: the formated string
+ * @...: Variable number of arguments
+ * Return: Number of characters printed (excluding the null byte)
  */
 
 int _printf(const char *format, ...)
@@ -14,20 +57,37 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	while (*format != '\0')
+	while (format && *format)
 	{
-		if (*format == '%')
-		{
-			format++;
-			printed = selector(format, args, printed);
-			format++;
-		}
-		else
+		if (*format != '%')
 		{
 			_putchar(*format);
 			printed++;
-			format++;
 		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			switch (*format)
+			{
+				case 'c':
+					printed += _print_char(args);
+					break;
+				case 's':
+					printed += _print_string(args);
+					break;
+				case '%':
+					_putchar('%');
+					printed++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(*format);
+					printed += 2;
+			}
+		}
+		format++;
 	}
 	va_end(args);
 	return (printed);
