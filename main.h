6b249950897345886197a307732a229951b96538
Moses-Nyagudi <nyagudi.ulridge@strathmore.edu>
2023-10-17 17:40:48 +0300

    updated README & Main.h

--- main.h
+++ main.h
@@ -7,5 +7,28 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_int(va_list args);
+
+
+int print_int_recursive(int num);
+int print_binary(va_list args);
+int print_hex(va_list args);
+int print_octal(va_list args);
+int print_unsigned(va_list args);
+int print_custom_string(va_list args, char *buffer, int *buffer_index);
+int print_pointer(va_list args, char *buffer, int *buffer_index);
+
+
+static void handle_rot13(char *str, int len);
+
+int handle_length_modifiers(const char *format, int i, char *length_modifier);
+int handle_field_width(const char *format, int i, int *field_width);
+int handle_flags(const char *format, int i, int *plus_flag, int *space_flag, int *hash_flag);
+int handle_precision(const char *format, int i, int *precision);
+int handle_zero_flag(const char *format, int i, int *zero_flag);
+int handle_minus_flag(const char *format, int i, int *minus_flag);
+int handle_custom_specifiers(va_list args, char *buffer, int *buffer_index, char specifier);
 
 #endif /* MAIN_H */
