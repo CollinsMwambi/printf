cfd8c4b3cf77427ae9cce8b06a29b3f1035e9a52
Moses-Nyagudi <nyagudi.ulridge@strathmore.edu>
2023-10-17 20:08:40 +0300

    swap function positions

--- 3-print_unsigned.c
+++ 3-print_unsigned.c
@@ -1,26 +1,4 @@
 #include "main.h"
-
-/**
- * print_unsigned - Print an unsigned integer
- * @args: The va_list containing the unsigned integer
- *
- * Return: Number of characters printed
- */
-
-int print_unsigned(va_list args)
-{
-	unsigned int num = va_arg(args, unsigned int);
-
-	int count = 0;
-
-	if (num / 10)
-		count += print_unsigned_recursive(num / 10);
-
-	_putchar('0' + (num % 10));
-	return (count + 1);
-
-}
-
 /**
  * print_unsigned_recursive - Helper function to print an unsigned integer
  * @num: The unsigned integer to print
@@ -28,13 +6,33 @@ int print_unsigned(va_list args)
  * Return: Number of characters printed
  *
  */
-int print_unsigned_recursive(unsigned int num)
+int print_unsigned_recursive(unsigned int num, char *buffer, int *buffer_index)
 {
 	int count = 0;
 
 	if (num / 10)
-		count += print_unsigned_recursive(num / 10);
+		count += print_unsigned_recursive(num / 10, buffer, buffer_index);
 
-	_putchar('0' + (num % 10));
+	_putchar('0' + (num % 10), buffer, buffer_index);
 	return (count + 1);
+}
+/**
+ * print_unsigned - Print an unsigned integer
+ * @args: The va_list containing the unsigned integer
+ *
+ * Return: Number of characters printed
+ */
+
+int print_unsigned(va_list args, char *buffer, int *buffer_index)
+{
+	unsigned int num = va_arg(args, unsigned int);
+
+	int count = 0;
+
+	if (num / 10)
+		count += print_unsigned_recursive(num / 10, buffer, buffer_index);
+
+	_putchar('0' + (num % 10), buffer, buffer_index);
+	return (count + 1);
+
 }
